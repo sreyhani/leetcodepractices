7754fef78de4c5987d64218feef1a4bcdaa735b4
sajjad <sreyhani@ce.sharif.edu>
2023-11-27 12:56:44 +0330

    use leading_zero, count_ones api's

--- rust/src/number_of_steps.rs
+++ rust/src/number_of_steps.rs
@@ -2,19 +2,9 @@ use crate::Solution;
 
 impl Solution {
     pub fn number_of_steps(num: i32) -> i32 {
-        let mut most_sign_one: i32 = 0;
-        let mut ones: i32 = 0;
-        for pos in (0..32).rev() {
-            let bit = (num >> pos) & 1;
-            match (most_sign_one, bit) {
-                (0, 0) => (),
-                (0, 1) => most_sign_one = pos + 1,
-                (_, 0) => (),
-                (_, 1) => ones += 1,
-                (_, _) => (),
-            }
+        match num {
+            0 => 0,
+            _ => 31 - num.leading_zeros() as i32 + num.count_ones() as i32,
         }
-
-        most_sign_one + ones
     }
 }
