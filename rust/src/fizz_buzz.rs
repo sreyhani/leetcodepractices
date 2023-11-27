d91408446d3cf288b98edfcd76260a35e22302ea
sajjad <sreyhani@ce.sharif.edu>
2023-11-27 12:14:08 +0330

    fizzbuzz completed

--- rust/src/fizz_buzz.rs
+++ rust/src/fizz_buzz.rs
@@ -2,6 +2,18 @@ use crate::Solution;
 
 impl Solution {
     pub fn fizz_buzz(n: i32) -> Vec<String> {
-        vec![n.to_string()]
+        let mut res = vec![];
+        for i in 1..=n {
+            if i % 15 == 0 {
+                res.push("FizzBuzz".to_string());
+            } else if i % 3 == 0 {
+                res.push("Fizz".to_string());
+            } else if i % 5 == 0 {
+                res.push("Buzz".to_string());
+            } else {
+                res.push(i.to_string());
+            }
+        }
+        res
     }
 }
--- rust/tests/fizz_buzz.rs
+++ rust/tests/fizz_buzz.rs
@@ -1,6 +1,28 @@
 use leetcodepractice::Solution;
 
 #[test]
-fn test_1() {
-    assert_eq!(Solution::fizz_buzz(1), ["1"])
+fn test_1_2() {
+    assert_eq!(Solution::fizz_buzz(1), ["1"]);
+    assert_eq!(Solution::fizz_buzz(2), ["1", "2"]);
+}
+
+#[test]
+fn test_3() {
+    assert_eq!(Solution::fizz_buzz(3), ["1", "2", "Fizz"]);
+}
+
+#[test]
+fn test_5() {
+    assert_eq!(Solution::fizz_buzz(5), ["1", "2", "Fizz", "4", "Buzz"]);
+}
+
+#[test]
+fn test_15() {
+    assert_eq!(
+        Solution::fizz_buzz(15),
+        [
+            "1", "2", "Fizz", "4", "Buzz", "Fizz", "7", "8", "Fizz", "Buzz", "11", "Fizz", "13",
+            "14", "FizzBuzz"
+        ]
+    );
 }
