49423457297520004f65b8dbc95b0a1eee00cbdb
sajjad <sreyhani@ce.sharif.edu>
2023-11-27 12:40:32 +0330

    impl number of steps

--- rust/src/number_of_steps.rs
+++ rust/src/number_of_steps.rs
@@ -2,6 +2,19 @@ use crate::Solution;
 
 impl Solution {
     pub fn number_of_steps(num: i32) -> i32 {
-        0
+        let mut most_sign_one: i32 = 0;
+        let mut ones: i32 = 0;
+        for pos in (0..32).rev() {
+            let bit = (num >> pos) & 1;
+            match (most_sign_one, bit) {
+                (0, 0) => (),
+                (0, 1) => most_sign_one = pos + 1,
+                (_, 0) => (),
+                (_, 1) => ones += 1,
+                (_, _) => (),
+            }
+        }
+
+        most_sign_one + ones
     }
 }
