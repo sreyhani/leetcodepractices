cb69f017682d3042e44c6a68b0f52d685d00c512
sajjad <sajjad7997@gmail.com>
2023-12-05 09:35:52 +0330

    fix lint

--- rust/src/middle_node.rs
+++ rust/src/middle_node.rs
@@ -1,5 +1,3 @@
-use core::panic;
-
 use crate::Solution;
 
 // Definition for singly-linked list.
@@ -9,20 +7,13 @@ pub struct ListNode {
     pub next: Option<Box<ListNode>>,
 }
 
-impl ListNode {
-    #[inline]
-    fn new(val: i32) -> Self {
-        ListNode { next: None, val }
-    }
-}
-
 impl Solution {
     pub fn middle_node(head: Option<Box<ListNode>>) -> Option<Box<ListNode>> {
         let len = Self::find_list_length(head.as_ref());
         let mid = len / 2 + 1;
         Self::find_node(head, mid)
     }
-
+    #[allow(clippy::borrowed_box)]
     fn find_list_length(head: Option<&Box<ListNode>>) -> u32 {
         match head {
             Some(node) => Self::find_list_length(node.next.as_ref()) + 1,
--- rust/tests/ransom_note.rs
+++ rust/tests/ransom_note.rs
@@ -2,24 +2,15 @@ use leetcodepractice::Solution;
 
 #[test]
 fn test_1() {
-    assert_eq!(
-        Solution::can_construct("a".to_string(), "b".to_string()),
-        false
-    )
+    assert!(!Solution::can_construct("a".to_string(), "b".to_string()))
 }
 
 #[test]
 fn test_2() {
-    assert_eq!(
-        Solution::can_construct("aa".to_string(), "ab".to_string()),
-        false
-    )
+    assert!(!Solution::can_construct("aa".to_string(), "ab".to_string()))
 }
 
 #[test]
 fn test_3() {
-    assert_eq!(
-        Solution::can_construct("aa".to_string(), "aab".to_string()),
-        true
-    )
+    assert!(Solution::can_construct("aa".to_string(), "aab".to_string()))
 }
